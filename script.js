3cc88a165559ec8dc94f64c2e9bcd1192a53e308
Kishen Kumar <kishendhoni@gmail.com>
2021-08-14 19:54:12 +0530

    Added new file test.html

--- script.js
+++ script.js
@@ -5,16 +5,17 @@ const desc = document.querySelector('#desc');
 const temp = document.querySelector('#temp');
  
 button.addEventListener('click',function(){
-    fetch("http://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&appid=15366dc2866888931ce2d3413791da6d")
+    fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&appid=15366dc2866888931ce2d3413791da6d")
         .then(response => response.json())
         .then(data => {
             const nameVal = data['name'];
             const descVal = data['weather'][0]['description'];
-            const tempVal = ((data['main']['temp'])-273.15).toFixed(2)+" °C";
+            const tempVal = ((data['main']['temp'])-273.15).toFixed(2);
+            const finalTemp = Math.round(tempVal)+"°C";
             
             cityname.innerHTML = nameVal;
             desc.innerHTML = descVal;
-            temp.innerHTML = tempVal;
+            temp.innerHTML = finalTemp;
         })
 	.catch(err=>alert('Enter a valid city name'))
 });
