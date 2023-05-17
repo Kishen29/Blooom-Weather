7c8663c6fcd306081b2b570f149da6d12447dfdc
Kishen29 <kishendhoni@gmail.com>
2023-05-17 15:46:26 +0530

    Bug Fixes

--- script.js
+++ script.js
@@ -3,19 +3,19 @@ const inputValue = document.querySelector('#inputValue');
 const cityname = document.querySelector('#name');
 const desc = document.querySelector('#desc');
 const temp = document.querySelector('#temp');
- 
-button.addEventListener('click',function(){
-    fetch("https://api.openweathermap.org/data/2.5/weather?q="+inputValue.value+"&appid=15366dc2866888931ce2d3413791da6d")
+
+button.addEventListener('click', function () {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + inputValue.value + "&appid=15366dc2866888931ce2d3413791da6d")
         .then(response => response.json())
         .then(data => {
             const nameVal = data['name'];
             const descVal = data['weather'][0]['description'];
-            const tempVal = ((data['main']['temp'])-273.15).toFixed(2);
-            const finalTemp = Math.round(tempVal)+"°C";
-            
+            const tempVal = ((data['main']['temp']) - 273.15).toFixed(2);
+            const finalTemp = Math.round(tempVal) + "°C";
+
             cityname.innerHTML = nameVal;
             desc.innerHTML = descVal;
             temp.innerHTML = finalTemp;
         })
-	.catch(err=>alert('Enter a valid city name'))
+        .catch(err => alert('Enter a valid city name'))
 });
